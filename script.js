a83669d24b4661b74da01bf57d6e8f4dfbd861a5
Sarkhan <abdullayevsarxan92@gmail.com>
2023-05-01 23:13:21 +0400

    last edition to project

--- script.js
+++ script.js
@@ -111,19 +111,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let saveBtn = document.querySelector('.save');
 
-    function saveImage() {
-        const image = canvas.toDataURL('image/png');
+    function saveImg() {
+        canvas.toBlob((blob) => {
+            let link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
 
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(image);
+            link.download = 'my_drawing.png';
 
-        link.download = 'my_drawing.png';
+            document.body.appendChild(link);
+            link.click();
 
-        document.body.appendChild(link);
-        link.click();
-
-        document.body.removeChild(link);
+            document.body.removeChild(link);
+        });
     }
 
-    saveBtn.addEventListener('click', saveImage);
+    saveBtn.addEventListener('click', saveImg);
 });
